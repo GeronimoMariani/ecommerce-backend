468e57e77a50d098d5e32195e67d8c9eda6ec44b
Geronimo Mariani <geronimomariani5@gmail.com>
2024-05-23 15:57:54 -0300

    Proyecto final

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,16 @@
+function modifyRol(id) {
+    axios.post(`http://localhost:8080/api/users/roles/${id}`)
+        .then(response => {
+            if (response.status === 200) {
+                alert('Rol actualizado');
+                location.reload();
+            } else {
+                alert('Error al actualizar el rol');
+            }
+        })
+        .catch(err => console.error('Error:', err));
+} 
+
 function deleteUser(id) {
     axios.delete(`http://localhost:8080/api/users/${id}`)
         .then(response => {
